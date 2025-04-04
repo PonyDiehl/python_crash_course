e171a010db911ea6e2636579a1674237ca55f8b3
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 09:40:03 -0600

    Create whips.py

---
+++ whips.py
@@ -0,0 +1,3 @@
+whips = ['bugatti', 'rarri', 'lambo', 'benzo']
+message = f"Ima wake up in my {whips[0].title()} and get my chain out of the {whips[1].title()} cause I rked the {whips[2].title()} and my girls got the {whips[3].title()} frfr "
+print(message)
