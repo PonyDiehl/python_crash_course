a5f5e55142f1f08adf8ee4972166be8a0f3885f4
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 09:59:34 -0600

    Update motorcycles.py

--- motorcycles.py
+++ motorcycles.py
@@ -10,10 +10,15 @@
 #motorcycles.append('ducati')
 #print(motorcycles)
 
-motorcycles = []
+#motorcycles = []
 
-motorcycles.append('honda')
-motorcycles.append('yamaha')
-motorcycles.append('suzuki')
+#motorcycles.append('honda')
+#motorcycles.append('yamaha')
+#motorcycles.append('suzuki')
 
+#print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+motorcycles.insert(0, 'ducati')
 print(motorcycles)
