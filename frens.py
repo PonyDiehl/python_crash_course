84f57dddc9502b4da0ce10ec6d952954b3b0a288
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 09:14:14 -0600

    Update frens.py

--- frens.py
+++ frens.py
@@ -1,2 +1,6 @@
+#frens = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+#print(frens[0],frens[1],frens[2],frens[3],frens[4])
+
 frens = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
-print(frens[0],frens[1],frens[2],frens[3],frens[4])
+message = f"What's up {frens[1]}, did you see {frens[0]}'s crazy new hat?!"
+print(message)
