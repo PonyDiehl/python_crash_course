9e704497ec7ad97dc197284600c80187ccbaacc0
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 10:13:31 -0600

    Update motorcycles.py

--- motorcycles.py
+++ motorcycles.py
@@ -42,7 +42,12 @@
 #print(motorcycles)
 #print(popped_motorcycle)
 
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+
+#last_owned = motorcycles.pop()
+#print(f"The last motorcycle I owned was a {last_owned.title()}")
+
 motorcycles = ['honda', 'yamaha', 'suzuki']
 
-last_owned = motorcycles.pop()
-print(f"The last motorcycle I owned was a {last_owned.title()}")
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}")
