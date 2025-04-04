d37bb36e602eaf6aa0839dae799820fde91d661c
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 10:19:38 -0600

    Update motorcycles.py

--- motorcycles.py
+++ motorcycles.py
@@ -47,7 +47,21 @@
 #last_owned = motorcycles.pop()
 #print(f"The last motorcycle I owned was a {last_owned.title()}")
 
-motorcycles = ['honda', 'yamaha', 'suzuki']
+#motorcycles = ['honda', 'yamaha', 'suzuki']
 
-first_owned = motorcycles.pop(0)
-print(f"The first motorcycle I owned was a {first_owned.title()}")
+#first_owned = motorcycles.pop(0)
+#print(f"The first motorcycle I owned was a {first_owned.title()}")
+
+#motorcycles = ['honda', 'yamaha', 'suzuki' 'ducati']
+#print(motorcycles)
+
+#motorcycles.remove('ducati')
+#print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me. But I still want one!")
