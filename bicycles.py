baab67055ad765b49271cb50d6b975767c3bae0b
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 08:57:31 -0600

    Update bicycles.py

--- bicycles.py
+++ bicycles.py
@@ -1,2 +1,8 @@
+#bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+#print(bicycles)
+
+#bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+#print(bicycles[0])
+
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles)
+print(bicycles[0].title())
