01ccbca89a97502434c4e247267b8df13f9d3bc7
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 07:33:56 -0600

    Update stripping_whitespace.py

--- stripping_whitespace.py
+++ stripping_whitespace.py
@@ -3,3 +3,12 @@ favorite_language
 
 favorite_language.rstrip()
 favorite_language
+
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+
+favorite_language = ' python '
+favorite_language.lstrip()
+favorite_language.strip()
+
+print (favorite_language)
