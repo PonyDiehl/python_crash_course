be154d4894faf98bd3dc70078b92b68161847831
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 10:06:52 -0600

    Update motorcycles.py

--- motorcycles.py
+++ motorcycles.py
@@ -29,8 +29,15 @@
 #del motorcycles[0]
 #print(motorcycles)
 
+#motorcycles = ['honda', 'yamaha', 'suzuki']
+#print(motorcycles)
+
+#del motorcycles[1]
+#print(motorcycles)
+
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 
-del motorcycles[1]
+popped_motorcycle = motorcycles.pop()
 print(motorcycles)
+print(popped_motorcycle)
