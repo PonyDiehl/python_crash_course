68e3c2bbc4cb9e3eb4e6c0182682b26082d74249
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 09:04:04 -0600

    Update bicycles.py

--- bicycles.py
+++ bicycles.py
@@ -11,5 +11,10 @@
 #print(bicycles[1])
 #print(bicycles[3])
 
+#bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+#print(bicycles[-1])
+
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles[-1])
+message = f"My first bike was a {bicycles[3].title()}"
+
+print(message)
