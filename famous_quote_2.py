62034f870eacd59d8e8ad0f1561f889a5c1855a5
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 08:35:35 -0600

    Update famous_quote_2.py

--- famous_quote_2.py
+++ famous_quote_2.py
@@ -1,3 +1,4 @@
+# This code prints a quote from Master Splinter
 name = 'Master Splinter'
 quote = "Darkness gives the ninja power, while light reveals the ninja's presence."
 message = f'{name} once said, "{quote}"'
