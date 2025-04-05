5408d692631ce14c225ec2fe30fec01042b7c185
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-05 03:34:12 -0600

    Update guest_list.py

--- guest_list.py
+++ guest_list.py
@@ -2,9 +2,25 @@
 #message = f"Hey, {guests[0]}, {guests[1]}, {guests[2]}, do you want to come to the dinner party?"
 #print(message)
 
-guests = ['tom', 'lisa', 'billy']
-guests.append('jerry')
-del guests[0]
-message = f"Hey, {guests[0]}, {guests[1]}, {guests[2]} do you want to come to the dinner party?"
+#guests = ['tom', 'lisa', 'billy']
+#guests.append('jerry')
+#message = f"Hey {guests[0]} can't make it, {guests[1]}, {guests[2]}, {guests[3]},do you want to come to the dinner party?"
+#print(message)
 
-print(message)
+#guests = ['tom', 'lisa', 'billy']
+#guests.append('jerry')
+#del guests[0]
+#message = f"Hey, {guests[0]}, {guests[1]}, {guests[2]} do you want to come to the dinner party?"
+#print(message)
+
+guests = ['tom','lisa','billy']
+guests.insert(0,'jerry')
+guests.insert(2,'josh')
+guests.append('chelsea')
+message1 = f"Hey, {guests[0]}, do you want to come to the dinner party?"
+message2 = f"Hey, {guests[1]}, do you want to come to the dinner party?"
+message3 = f"Hey, {guests[2]}, do you want to come to the dinner party?"
+message4 = f"Hey, {guests[3]}, do you want to come to the dinner party?"
+message5 = f"Hey, {guests[4]}, do you want to come to the dinner party?"
+message6 = f"Hey, {guests[5]}, do you want to come to the dinner party?"
+print(message1, message2, message3, message4, message5, message6)
