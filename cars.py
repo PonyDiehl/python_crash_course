306f1d91b9664a78284176019d3ed4442cc5bc38
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-05 04:16:49 -0600

    Create cars.py

---
+++ cars.py
@@ -0,0 +1,36 @@
+#Sorting a list permanently with .sort() method
+# The .sort() method sorts the list in alphabetical order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+# .sort(reverse=True) will sort the list in reverse alphabetical order
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+
+# Sorting a list temporarily with the sorted() function
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list:")
+print(cars)
+
+print ("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+# Reversing the order of a list with the reverse() method
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+print("\nHere is the reversed list:")
+cars.reverse()
+print(cars)
+
+# Finding the length of a list with the len() function
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
