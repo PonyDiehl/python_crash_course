06dcc94e4d0a3a172d769d8b990e28b9aae42d02
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-05 08:15:23 -0600

    Update magicians.py

--- magicians.py
+++ magicians.py
@@ -6,3 +6,4 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
     print(f"I can't wait to see your nxt trick, {magician.title()}.\n")
+print("Thank you, everyone. That was a great magic show!")
