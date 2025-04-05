11af94022d3eecdef908f3cc8a17a5f3f310abe0
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-05 10:48:58 -0600

    Update players.py

--- players.py
+++ players.py
@@ -6,3 +6,9 @@ print(players[1:4])  # Middle three players
 print(players[:4]) # If the first index is omitted, it defaults to the beginning of the list
 print(players[2:]) # If the last index is omitted, it defaults to the end of the list
 print (players[-3:]) # Last three players
+
+# Looping through a slice
+
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
