b9a5274a2b28f002fe5c5c349c4c9c144602fe7a
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-05 10:16:10 -0600

    Create summing_a_million.py

---
+++ summing_a_million.py
@@ -0,0 +1,5 @@
+digits = [value for value in range(1, 1000001)]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+# The above code creates a list of numbers from 1 to 1,000,000 and prints the minimum, maximum, and sum of the numbers.
