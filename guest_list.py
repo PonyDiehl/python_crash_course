f75b93865f3ababd6255928ddef64cd3f0cc2917
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-04 22:16:39 -0600

    Update guest_list.py

--- guest_list.py
+++ guest_list.py
@@ -1,3 +1,10 @@
+#guests = ['tom', 'lisa', 'billy']
+#message = f"Hey, {guests[0]}, {guests[1]}, {guests[2]}, do you want to come to the dinner party?"
+#print(message)
+
 guests = ['tom', 'lisa', 'billy']
-message = f"Hey, {guests[0]}, {guests[1]}, {guests[2]}, do you want to come to the dinner party?"
+guests.append('jerry')
+del guests[0]
+message = f"Hey, {guests[0]}, {guests[1]}, {guests[2]} do you want to come to the dinner party?"
+
 print(message)
