4a24942a9598f526b188f129ed5a98048c430a4e
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-05 06:23:12 -0600

    Update every_function_chapter_3.py

--- every_function_chapter_3.py
+++ every_function_chapter_3.py
@@ -1,4 +1,14 @@
 things = ["phone", "laptop", "tablet", "camera", "watch"]
-people = ["Alice", "Bob", "Charlie", "David", "Eve"]
+people = ["Alice", "David", "Zach", "Bob", "Eve"]
 places = ["mountain", "beach", "forest", "desert", "city"]
 
+message = f'Hi {people[0].title ()}, I do you want to go the the {places[1]} with me today? Be sure to bring your {things[3]} with you!'
+print (message)
+
+people.append("Frank")
+
+message2 = f'If {people} want to go as well feel free to invite them!'
+print(message2)
+
+message3 = f'I know {people[4]} is more of a city person, but I think they would enjoy it too, we are going to go to the {places[0]} after dinner.'
+print(message3)
