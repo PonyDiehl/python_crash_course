3e3078d38cbf5513c460efb00b72bbf43d9c6510
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-05 04:38:18 -0600

    Update seeing_the_world.py

--- seeing_the_world.py
+++ seeing_the_world.py
@@ -1,3 +1,16 @@
 world = ["Miami", "Brisbane", "Paris", "Tokyo", "New York", "London", "Berlin", "Madrid", "Rome", "Sydney"]
 print(world)
 
+# sorted() function sorts the list in alphabetical order but does not change the original list
+
+print("\nHere is the sorted list:")
+print(sorted(world))
+
+# The original list remains unchanged
+
+print(world)
+
+print("\nHere is the sorted list in reverse order:")
+print(sorted(world, reverse=True))
+
+print(sorted(world, reverse=False))
