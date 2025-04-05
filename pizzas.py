f0224ddfc9eb49b845f7071f40d051e3a3c0416d
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-05 09:01:30 -0600

    Update pizzas.py

--- pizzas.py
+++ pizzas.py
@@ -1,4 +1,6 @@
 pizzas =['meat lovers', 'hawaiian', 'buffalo chicken', 'supreme']
 for pizza in pizzas:
     print(pizza)
-    print(f"I like {pizza} pizaa and a pint!")
+    print(f"I like {pizza} pizza and a pint!\n")
+message = f"The best pizza is {pizzas[1].title()}!"
+print(message)
