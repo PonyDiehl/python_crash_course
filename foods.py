8e9a6a591851799fc8e2c80a19e58309b07430f8
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 00:01:13 -0600

    Update foods.py

--- foods.py
+++ foods.py
@@ -1,2 +1,10 @@
 # Copying a list
 
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+print ("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
