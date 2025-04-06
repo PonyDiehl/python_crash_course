29bf6a9dd02fa5d2597d86aa6a0adedfc4a67687
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 08:29:57 -0600

    Update hello_admin.py

--- if_statements/hello_admin.py
+++ if_statements/hello_admin.py
@@ -1,11 +1,11 @@
 users = ['admin', 'jaden', 'kristi', 'todd', 'tina']
 if 'admin' in users:
     print(f"Hello Admin, would you like to see a status report?")
-if  users:
+if  users[1]:
     print(f"Hello {users[1]}, thanks for logging in!")
-if  users:
+if  users[2]:
     print(f"Hello {users[2]}, thanks for logging in!")
-if  users:
+if  users[3]:
     print(f"Hello {users[3]}, thanks for logging in!")
-if  users:
+if  users[4]:
     print(f"Hello {users[4]}, thanks for logging in!")
