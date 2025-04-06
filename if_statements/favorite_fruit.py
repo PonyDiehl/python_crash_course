9605c33e32e83c1fe6f2fd2173085a63654c037f
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 06:58:42 -0600

    Update favorite_fruit.py

---
+++ if_statements/favorite_fruit.py
@@ -0,0 +1,11 @@
+favorite_fruit = ['pomegranate', 'mango', 'kiwi']
+if favorite_fruit == 'pomegranate':
+    print("\nI really like pomegranate!")
+if favorite_fruit == 'apples':
+    print("\nI really like apples!")
+if favorite_fruit == 'mango':
+    print("\nI really like mango!")
+if favorite_fruit == 'strawberrys':
+    print("\nI really like strawberrys!")
+if favorite_fruit == 'kiwi':
+    print("\nI really like kiwi!")
