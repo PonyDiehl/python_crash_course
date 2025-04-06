096b91dfe2079c826f7c7bbade726d499a15550b
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 06:25:30 -0600

    Update alien_colors1.py

--- if_statements/alien_colors1.py
+++ if_statements/alien_colors1.py
@@ -16,4 +16,20 @@ if alien_color == 'green':
 elif alien_color == 'yellow':
     print('\nYou just earned 10 points!')
 else:
+    print('\nYou just earned 15 points!')
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('\nYou just earned 5 points!')
+elif alien_color == 'yellow':
     print('\nYou just earned 10 points!')
+else:
+    print('\nYou just earned 15 points!')
+
+alien_color = 'red'
+if alien_color == 'green':
+    print('\nYou just earned 5 points!')
+elif alien_color == 'yellow':
+    print('\nYou just earned 10 points!')
+else:
+    print('\nYou just earned 15 points!')
