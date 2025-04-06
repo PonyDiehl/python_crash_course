9b2af1aa294d8ad7a5c01609b6d2a88d37ce2adf
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 02:31:17 -0600

    Create toppings.py

---
+++ if_statements/toppings.py
@@ -0,0 +1,12 @@
+# Checking for Inequality. 
+# Equality opperator is '=='
+# Inequality opperator is '!='
+
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+requested_topping = 'anchovies'
+if requested_topping == 'anchovies':
+    print("Gimme them fish!")
