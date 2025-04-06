67ec9e4f13b0a6fbb614a1d92364e36b810547e3
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 07:44:48 -0600

    Update toppings.py

--- if_statements/toppings.py
+++ if_statements/toppings.py
@@ -33,3 +33,13 @@ for requested_topping in requested_toppings:
     print(f"Adding {requested_topping}")
 
 print('\nFinished making your pizza!')
+
+# Using if statements inside a loop 
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else: 
+        print((f"Adding {requested_topping}."))
+print(f"Finished making your pizza!")
