b45e867b726ec3232cea584d35d7846e80539422
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 04:42:32 -0600

    Update voting.py

--- if_statements/voting.py
+++ if_statements/voting.py
@@ -1,4 +1,15 @@
 # Simple if statemests
-are = 19
-if are >= 18:
+age = 19
+if age >= 18:
     print('You can vote')
+age = 19 
+if age >= 18:
+    print('You can vote')
+    print('You are an adult')
+    print('Now you have to pay taxes')
+# if else statements
+age = 17 
+if age >= 18: 
+    print('You can vote')
+else:
+    print('You are not old enough to vote')
