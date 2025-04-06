61a1737475ed6a5c37df41c07d65fe22ea69b577
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 06:41:47 -0600

    Update stages_of_life.py

--- if_statements/stages_of_life.py
+++ if_statements/stages_of_life.py
@@ -11,3 +11,73 @@ elif age <=65:
     print("\nThat's an adult!")
 else:
     print("\nThat's an elder!")
+
+age = 3
+if age <= 2:
+    print("\nThat's a baby!")
+elif age <= 4:
+    print("\nThat's a toddler!")
+elif age <= 13:
+    print("\nThat's a kid!")
+elif age <=20:
+    print("\nThats' a teenager!")
+elif age <=65:
+    print("\nThat's an adult!")
+else:
+    print("\nThat's an elder!")
+
+age = 7
+if age <= 2:
+    print("\nThat's a baby!")
+elif age <= 4:
+    print("\nThat's a toddler!")
+elif age <= 13:
+    print("\nThat's a kid!")
+elif age <=20:
+    print("\nThats' a teenager!")
+elif age <=65:
+    print("\nThat's an adult!")
+else:
+    print("\nThat's an elder!")
+
+age = 14
+if age <= 2:
+    print("\nThat's a baby!")
+elif age <= 4:
+    print("\nThat's a toddler!")
+elif age <= 13:
+    print("\nThat's a kid!")
+elif age <=20:
+    print("\nThats' a teenager!")
+elif age <=65:
+    print("\nThat's an adult!")
+else:
+    print("\nThat's an elder!")
+
+age = 22
+if age <= 2:
+    print("\nThat's a baby!")
+elif age <= 4:
+    print("\nThat's a toddler!")
+elif age <= 13:
+    print("\nThat's a kid!")
+elif age <=20:
+    print("\nThats' a teenager!")
+elif age <=65:
+    print("\nThat's an adult!")
+else:
+    print("\nThat's an elder!")
+
+age = 77
+if age <= 2:
+    print("\nThat's a baby!")
+elif age <= 4:
+    print("\nThat's a toddler!")
+elif age <= 13:
+    print("\nThat's a kid!")
+elif age <=20:
+    print("\nThats' a teenager!")
+elif age <=65:
+    print("\nThat's an adult!")
+else:
+    print("\nThat's an elder!")
