7b964ac39782f7dab5bbffcf6d8cf0e7ca6516e5
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 03:11:24 -0600

    Update conditional_tests.py

--- if_statements/conditional_tests.py
+++ if_statements/conditional_tests.py
@@ -9,7 +9,21 @@ print(car == 'audi\n')  # False
 
 dog = 'golden retriever'
 print("Is the dog a 'golden retriever'? I predict True.")
-print(dog == 'golden retriever')  # True
+print(dog == 'golden retriever\n')  # True
 
 print("\nIs the dog a 'poodle'? I predict False.")
 print(dog == 'poodle\n')  # False
+
+bro = 'chad'
+print("Is the bro 'chad'? I predict True.")
+print(bro == 'chad\n')  # True
+
+print("\nIs the bro 'soy'? I predict False.")
+print(bro == 'soy\n')  # False
+
+frute = 'banana'
+print("Is the frute 'banana'? I predict True.")
+print(frute == 'banana\n')  # True
+
+print("\nIs the frute 'apple'? I predict False.")
+print(frute == 'apple\n')  # False
