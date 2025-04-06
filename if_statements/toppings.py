b8af0738c042ca93d18e27b34e7706f969f30ae3
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 05:52:01 -0600

    Update toppings.py

--- if_statements/toppings.py
+++ if_statements/toppings.py
@@ -10,3 +10,13 @@ if requested_topping != 'anchovies':
 requested_topping = 'anchovies'
 if requested_topping == 'anchovies':
     print("Gimme them fish!")
+# Testing mutiple conditions 
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print('Adding mushrooms')
+if 'pepperoni' in requested_toppings:
+    print('Adding pepperoni.')
+if 'extra cheese' in requested_toppings:
+    print('Adding extra cheese')
+print("\nFinished maiking your pizza")
