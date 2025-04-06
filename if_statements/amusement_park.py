891812aeae4b0afcd48c6018cf225236c2fd44e2
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 05:09:58 -0600

    Update amusement_park.py

--- if_statements/amusement_park.py
+++ if_statements/amusement_park.py
@@ -26,3 +26,14 @@ elif age < 65:
 else: 
     price = 30
 print(f"Your ticket price is ${price}")
+# Ommitting the else statement
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 50
+elif age >= 65:
+    price = 30
+print(f"Your ticket price is ${price}")
