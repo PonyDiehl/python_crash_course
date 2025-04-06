830f595137cb9909751f86639c3e2715a8346b09
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-06 06:38:53 -0600

    Create stages_of_life.py

---
+++ if_statements/stages_of_life.py
@@ -0,0 +1,13 @@
+age = 1
+if age <= 2:
+    print("\nThat's a baby!")
+elif age <= 4:
+    print("\nThat's a toddler!")
+elif age <= 13:
+    print("\nThat's a kid!")
+elif age <=20:
+    print("\nThats' a teenager!")
+elif age <=65:
+    print("\nThat's an adult!")
+else:
+    print("\nThat's an elder!")
