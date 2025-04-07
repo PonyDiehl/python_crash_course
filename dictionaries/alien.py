263648c56e7ad5f72ad874fdd11e4e33bf6bc78c
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-07 10:51:20 -0600

    Update alien.py

--- dictionaries/alien.py
+++ dictionaries/alien.py
@@ -1,9 +1,18 @@
 # Sample dictionary
-
 alien_0 = {'color': 'green', 'points': 5}
 
 print(alien_0['color'])
 print(alien_0['points'])
 
+# Accessing values in a dictionary 
+
 new_points = alien_0['points']
 print(f"You just earned {new_points} points!")
+
+# Adding new key-value pairs
+
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
