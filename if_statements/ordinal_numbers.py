6c6c3b92c63a267a97402b1952965087616d5897
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-07 10:19:52 -0600

    Update ordinal_numbers.py

--- if_statements/ordinal_numbers.py
+++ if_statements/ordinal_numbers.py
@@ -1,11 +1,11 @@
 numbers = [1,2,3,4,5,6,7,8,9]
-
-if numbers == 1:
-    print(f'{numbers}st')
-elif numbers == 2:
-    print(f'{numbers}nd')
-elif numbers == 3:
-    print(f'{numbers}rd')
-else:
-    print(f'{numbers}rd')
+for number in numbers:
+    if number == 1:
+        print(f'{number}st')
+    elif number == 2:
+        print(f'{number}nd')
+    elif number == 3:
+        print(f'{number}rd')
+    else:
+        print(f'{number}rd')
 
