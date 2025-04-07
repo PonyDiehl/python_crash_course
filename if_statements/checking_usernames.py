2117f9bcb5cabfa36eb1e6b657b1f16b4b53043e
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2025-04-07 09:51:32 -0600

    Update checking_usernames.py

--- if_statements/checking_usernames.py
+++ if_statements/checking_usernames.py
@@ -1,8 +1,9 @@
+current_users = ['Mike', 'Sarah', 'Kim', 'Tom', 'Beath']
 current_users = ['mike', 'sarah', 'kim', 'tom','beath']
 new_users = ['kelly','jim','dug','sean','kim']
 
 for new_user in new_users:
     if new_user in current_users:
-        print(f"Username {new_user} is not avalible, enter new user name")
+        print(f"Username {new_user} is not avalible, enter new user name.")
     else:
-        print(f'Username {new_user} avalible')
+        print(f'Username {new_user} avalible.')
