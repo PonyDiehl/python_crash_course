59493f6682de0dfb03d8f55dbbabba18bf91f766
PonyDiehl <126234705+PonyDiehl@users.noreply.github.com>
2026-03-25 18:35:01 -0700

    Update favorite_languages.py

--- dictionaries/favorite_languages.py
+++ dictionaries/favorite_languages.py
@@ -7,14 +7,19 @@ favorite_languages = {
     'phill': 'python',
 }
 
-language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}.")
+# language = favorite_languages['sarah'].title()
+# print(f"Sarah's favorite language is {language}.")
 
-for name, language in favorite_languages.items():
-    print(f"{name.title()}'s favorite language is {language.title()}.")
-for name in favorite_languages.keys():
-    print(name.title())
+# for name, language in favorite_languages.items():
+#     print(f"{name.title()}'s favorite language is {language.title()}.")
+
+# for name in favorite_languages.keys():
+#     print(name.title())
 
 friends = ['phill', 'sarah']
-for name in favorite_languages:
-    print(f"\t{name.title()}, I see you love {language}!")
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
